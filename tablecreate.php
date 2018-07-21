90730900b41a285004434cd1e8c63a5b520f8bd9
paripb <paripb@gmail.com>
2018-07-21 16:55:58 +0530

    Update tablecreate.php

--- tablecreate.php
+++ tablecreate.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'tt1.database.windows.net';
-$username = 'paripb@tt1.sql.database.windows.net';
+$username = 'paripb@tt1.mysql.database.windows.net';
 $password = 'Apple@123';
 $db_name = 'tt1';
 
