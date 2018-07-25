d7643a31a83d1343618a443908eb6483ea6cf963
paripb <paripb@gmail.com>
2018-07-25 21:29:34 +0530

    Create dbcreate.php

---
+++ dbcreate.php
@@ -0,0 +1,22 @@
+<?php
+$host = 'tt2.mysql.database.azure.com';
+$username = 'paripb@tt2';
+$password = 'Apple@123';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// Create database
+$sql = "CREATE DATABASE tt2";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
