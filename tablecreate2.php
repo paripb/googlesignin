dd282c5ce4390de12888e9f2c82a8b3674741a61
paripb <paripb@gmail.com>
2018-07-26 00:26:38 +0530

    Update tablecreate2.php

--- tablecreate2.php
+++ tablecreate2.php
@@ -2,7 +2,7 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:tt1.database.windows.net,1433; Database = tt1", "paripb", "Apple@12");
+    $conn = new PDO("sqlsrv:server = tcp:tt1.database.windows.net,1433; Database = tt1", "paripb", "Apple@123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     print("Try");
 }
@@ -10,6 +10,14 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "paripb@tt1", "pwd" => "Apple@123", "Database" => "tt1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:tt1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
 
 // Run the create table query
 if (sqlsrv_query($conn, '
@@ -24,9 +32,5 @@ PRIMARY KEY (`Id`)
 printf("Table created\n");
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "paripb@tt1", "pwd" => "{your_password_here}", "Database" => "tt1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:tt1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
 ?>
+
