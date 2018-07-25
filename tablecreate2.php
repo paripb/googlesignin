f772583324bae23442338ffdf3837694eba00953
paripb <paripb@gmail.com>
2018-07-26 00:16:44 +0530

    Update tablecreate2.php

--- tablecreate2.php
+++ tablecreate2.php
@@ -11,6 +11,19 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
+// Run the create table query
+if (sql_query($conn, '
+CREATE TABLE Products (
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
+}
+
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "paripb@tt1", "pwd" => "{your_password_here}", "Database" => "tt1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:tt1.database.windows.net,1433";
